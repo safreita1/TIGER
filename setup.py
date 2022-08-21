d95a03bf5e0cdf6ca3320b1ea001793fffc7e21a
Scott Freitas <safreita1@gmail.com>
2022-08-20 17:47:59 -0700

    Testing support for multiple Python versions

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import find_packages, setup
 
-setup_requires = ["numpy<=1.22.4", "pandas", "Cython", "pythran", "scikit-image"]
+setup_requires = ["numpy==1.22.4", "pandas", "Cython", "pythran", "scikit-image"]
 
 install_requires = ["joblib",
                     "networkx",
@@ -10,7 +10,7 @@ install_requires = ["joblib",
                     "six",
                     "pillow",
                     "numba",
-                    "matplotlib<=3.6",
+                    "matplotlib==3.6",
                     "fa2",
                     "onnx==1.10.0",
                     "PyYAML",
